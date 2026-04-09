c3812ae194361ffa9c8baea69e31d3bb7e075d6a
Lee Raulin <leeraulin@gmail.com>
2026-04-09 18:39:08 -0400

    Add character data for Chris Redfield, Jill Valentine, Leon Kennedy, and T-Virus Zombie

--- scripts/main_setup.gd
+++ scripts/main_setup.gd
@@ -6,44 +6,105 @@ func _ready() -> void:
 	var manager: GameManager = $GameManager
 	print("=== GURPS Tactical Setup ===")
 
-	# --- Leon ---
-	var leon: CharacterData = _make_character("Leon", 11, 12, 10, 11, 11, 6.0, 5, 2)
-	leon.skills = {"Guns (Pistol)": 13, "Knife": 11}
-	leon.ranged_weapons.append(_make_pistol("M1911A1", "2d pi", 2, 150, 1850, 3, "7+1(3)", 13, 10, -2, 4))
-	leon.melee_weapons.append(_make_melee("Combat Knife", "Thrust", "1d imp", "C,1", 11, "0"))
-	var leon_token: CharacterToken = _make_token(leon, Color.CORNFLOWER_BLUE)
-	manager.add_character(leon_token, Vector2i(0, 0))
+	# --- Chris Redfield ---
+	var chris: CharacterData = _make_character("Chris Redfield", 13, 13, 11, 12, 13, 6.25, 6, 4)
+	chris.will = 11
+	chris.per = 11
+	chris.fp = 14
+	chris.fp_max = 14
+	chris.advantages = ["Appearance (Attractive)", "Combat Reflexes", "Fearlessness 2", "Fit",
+			"High Pain Threshold", "Legal Enforcement Powers 1", "Luck", "Resistant (Disease +3)"]
+	chris.disadvantages = ["Code of Honor (Soldier's)", "Duty (STARS)", "Overconfidence"]
+	chris.skills = {
+		"Brawling": 15, "Climbing": 12, "First Aid": 11, "Forced Entry": 15,
+		"Guns (Grenade Launcher)": 13, "Guns (Pistol)": 15, "Guns (Rifle)": 14,
+		"Guns (Shotgun)": 16, "Guns (Submachine Gun)": 14, "Knife": 13,
+		"Observation": 10, "Parachuting": 13, "Piloting (Helicopter)": 14,
+		"Piloting (Light Airplane)": 13, "Running": 11, "Savoir-Faire (Military)": 11,
+		"Soldier": 10, "Swimming": 12, "Tactics": 11, "Throwing": 12
+	}
+	chris.ranged_weapons.append(_make_pistol("Auto Pistol 9mm", "2d+2 pi", 2, 150, 1850, 3, "15+1(3)", 15, 9, -2, 2))
+	chris.melee_weapons.append(_make_melee("Large Knife", "Swing", "2d-3 cut", "C,1", 13, "0"))
+	chris.melee_weapons.append(_make_melee("Large Knife", "Thrust", "1d imp", "C,1", 13, "0"))
+	chris.melee_weapons.append(_make_melee("Brawling", "Punch", "1d cr", "C", 15, "0"))
+	chris.melee_weapons.append(_make_melee("Brawling", "Kick", "1d+1 cr", "C,1", 13, "No"))
+	var chris_token: CharacterToken = _make_token(chris, Color.CORNFLOWER_BLUE)
+	manager.add_character(chris_token, Vector2i(0, 0))
 
-	# --- Claire ---
-	var claire: CharacterData = _make_character("Claire", 10, 11, 10, 10, 10, 5.5, 5, 1)
-	claire.skills = {"Guns (Pistol)": 12}
-	claire.ranged_weapons.append(_make_pistol("Browning HP", "2d+2 pi+", 2, 175, 1900, 3, "13+1(3)", 12, 10, -2, 3))
-	var claire_token: CharacterToken = _make_token(claire, Color.INDIAN_RED)
-	manager.add_character(claire_token, Vector2i(2, -1))
-
-	# --- Jill ---
-	var jill: CharacterData = _make_character("Jill", 10, 13, 11, 11, 10, 6.0, 6, 1)
-	jill.skills = {"Guns (Pistol)": 14, "Knife": 12}
-	jill.ranged_weapons.append(_make_pistol("Beretta 92", "2d+2 pi", 2, 150, 1850, 3, "15+1(3)", 14, 10, -2, 4))
-	jill.melee_weapons.append(_make_melee("Combat Knife", "Thrust", "1d imp", "C,1", 12, "0"))
+	# --- Jill Valentine ---
+	var jill: CharacterData = _make_character("Jill Valentine", 8, 13, 12, 11, 8, 6.0, 6, 0)
+	jill.will = 12
+	jill.per = 12
+	jill.fp = 11
+	jill.fp_max = 11
+	jill.advantages = ["Appearance (Attractive)", "Combat Reflexes", "Fearlessness 2", "Fit",
+			"High Pain Threshold", "Luck"]
+	jill.disadvantages = ["Code of Honor (Soldier's)", "Duty (STARS)", "Overconfidence"]
+	jill.skills = {
+		"Brawling": 13, "Climbing": 12, "Computer Operation": 12, "Driving (Automobile)": 12,
+		"Electronics Repair (Security)": 13, "Explosives (Demolition)": 13, "Explosives (EOD)": 13,
+		"First Aid": 12, "Forced Entry": 13, "Guns (Grenade Launcher)": 13, "Guns (Pistol)": 13,
+		"Guns (Rifle)": 13, "Guns (Shotgun)": 13, "Guns (Submachine Gun)": 13,
+		"Knife": 13, "Lockpicking": 14, "Running": 10, "Savoir-Faire (Military)": 12,
+		"Soldier": 11, "Swimming": 11, "Throwing": 12
+	}
+	jill.ranged_weapons.append(_make_pistol("Beretta 92", "2d+2 pi", 2, 150, 1850, 3, "15+1(3)", 13, 9, -2, 2))
+	jill.melee_weapons.append(_make_melee("Large Knife", "Swing", "2d-3 cut", "C,1", 13, "0"))
+	jill.melee_weapons.append(_make_melee("Large Knife", "Thrust", "1d imp", "C,1", 13, "0"))
+	jill.melee_weapons.append(_make_melee("Brawling", "Punch", "1d-3 cr", "C", 13, "0"))
+	jill.melee_weapons.append(_make_melee("Brawling", "Kick", "1d-2 cr", "C,1", 11, "No"))
 	var jill_token: CharacterToken = _make_token(jill, Color.MEDIUM_SEA_GREEN)
 	manager.add_character(jill_token, Vector2i(-1, 2))
 
-	# --- Zombie A ---
-	var zombie_a: CharacterData = _make_character("Zombie A", 13, 8, 2, 10, 15, 4.5, 4, 2)
-	zombie_a.advantages = ["Injury Tolerance (No Brain)", "Injury Tolerance (No Vitals)", "High Pain Threshold"]
-	zombie_a.melee_weapons.append(_make_melee("Bite", "Thrust", "1d-1 cr", "C", 10, "No"))
-	zombie_a.melee_weapons.append(_make_melee("Claws", "Swing", "1d cut", "C", 9, "No"))
-	var za_token: CharacterToken = _make_token(zombie_a, Color.DARK_OLIVE_GREEN)
-	manager.add_character(za_token, Vector2i(4, -2))
+	# --- Leon Kennedy (RE4) ---
+	var leon: CharacterData = _make_character("Leon Kennedy", 13, 15, 12, 14, 13, 7.25, 7, 4)
+	leon.will = 14
+	leon.per = 13
+	leon.fp = 14
+	leon.fp_max = 14
+	leon.advantages = ["Appearance (Handsome)", "Combat Reflexes", "Danger Sense", "Fearlessness 3",
+			"Fit (Very Fit)", "Gunslinger", "Hard to Kill 3", "High Pain Threshold", "Luck",
+			"Patron (US Govt/DSO)", "Security Clearance"]
+	leon.disadvantages = ["Code of Honor (Hero's)", "Duty (US Govt/DSO)", "Overconfidence",
+			"Secret Identity", "Sense of Duty (Innocents)"]
+	leon.skills = {
+		"Acrobatics": 16, "Brawling": 15, "Climbing": 14, "Driving (Automobile)": 16,
+		"Fast-Draw (Pistol)": 16, "Forced Entry": 16, "Guns (Grenade Launcher)": 16,
+		"Guns (Light Machine Gun)": 17, "Guns (Pistol)": 19, "Guns (Rifle)": 19,
+		"Guns (Shotgun)": 18, "Guns (Submachine Gun)": 17, "Judo": 16, "Jumping": 16,
+		"Karate": 16, "Knife": 18, "Stealth": 16, "Tactics": 13,
+		"Throwing": 16, "Wrestling": 17
+	}
+	var leon_pistol: RangedWeaponData = _make_pistol("Auto Pistol 9mm", "2d+2 pi", 2, 150, 1850, 3, "15+1(3)", 19, 9, -2, 2)
+	leon.ranged_weapons.append(leon_pistol)
+	var leon_shotgun: RangedWeaponData = _make_pistol("Auto Shotgun 12G (Slug)", "5d pi++", 3, 125, 188, 3, "6+1(3)", 18, 10, -5, 4)
+	leon.ranged_weapons.append(leon_shotgun)
+	var leon_rifle: RangedWeaponData = _make_pistol("Bolt Rifle 7.62mm", "7d pi", 7, 1000, 4200, 1, "5+1(3)", 19, 10, -5, 4)
+	leon.ranged_weapons.append(leon_rifle)
+	leon.melee_weapons.append(_make_melee("Large Knife", "Swing", "2d-3 cut", "C,1", 18, "0"))
+	leon.melee_weapons.append(_make_melee("Large Knife", "Thrust", "1d imp", "C,1", 18, "0"))
+	leon.melee_weapons.append(_make_melee("Karate", "Punch", "1d+1 cr", "C", 16, "0"))
+	leon.melee_weapons.append(_make_melee("Karate", "Kick", "1d+2 cr", "C,1", 14, "No"))
+	var leon_token: CharacterToken = _make_token(leon, Color.DARK_ORANGE)
+	manager.add_character(leon_token, Vector2i(2, -1))
 
-	# --- Zombie B ---
-	var zombie_b: CharacterData = _make_character("Zombie B", 12, 8, 2, 10, 14, 4.5, 3, 2)
-	zombie_b.advantages = ["Injury Tolerance (No Brain)", "Injury Tolerance (No Vitals)", "High Pain Threshold"]
-	zombie_b.melee_weapons.append(_make_melee("Bite", "Thrust", "1d-1 cr", "C", 10, "No"))
-	zombie_b.melee_weapons.append(_make_melee("Claws", "Swing", "1d-1 cut", "C", 9, "No"))
-	var zb_token: CharacterToken = _make_token(zombie_b, Color.DARK_OLIVE_GREEN)
-	manager.add_character(zb_token, Vector2i(3, 2))
+	# --- T-Virus Zombie ---
+	var zombie: CharacterData = _make_character("T-Virus Zombie", 13, 8, 3, 8, 18, 4.0, 3, 0)
+	zombie.will = 3
+	zombie.per = 3
+	zombie.fp = 8
+	zombie.fp_max = 8
+	zombie.advantages = ["Fearlessness 5", "Hard to Kill 1", "Hard to Subdue 2",
+			"High Pain Threshold", "Resistant (Disease Immunity)", "Unfazeable"]
+	zombie.disadvantages = ["Appearance (Monstrous)", "Bad Smell", "Bestial", "Cannot Learn",
+			"Cannot Speak", "Compulsive Behavior (Eat Flesh)", "Infectious Attack",
+			"Odious Personal Habit (Constant moaning)", "Short Attention Span"]
+	zombie.skills = {"Brawling": 13}
+	zombie.melee_weapons.append(_make_melee("Brawling", "Punch", "1d cr", "C", 13, "0"))
+	zombie.melee_weapons.append(_make_melee("Brawling", "Bite", "1d cr", "C", 13, "No"))
+	zombie.melee_weapons.append(_make_melee("Brawling", "Kick", "1d+1 cr", "C,1", 11, "No"))
+	var zombie_token: CharacterToken = _make_token(zombie, Color.DARK_OLIVE_GREEN)
+	manager.add_character(zombie_token, Vector2i(4, -2))
 
 	print("Total characters: %d" % manager.characters.size())
 	manager.start_combat()
